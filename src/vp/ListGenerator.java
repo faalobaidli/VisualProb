37bbfd259ddee0aa67b07dbe2d660954d812ec1f
faalobaidli <fahad@eduroam-38-208.uea.ac.uk>
2014-11-06 15:09:53 +0000

    aaaa2

--- src/vp/ListGenerator.java
+++ src/vp/ListGenerator.java
@@ -104,11 +104,7 @@ public class ListGenerator {
 		}
 	}
 
-<<<<<<< Updated upstream
-	///// Formula list generators
-=======
-	
->>>>>>> Stashed changes
+
 	public List<String> intersection(List<String> leftEvent, List<String> rightEvent){
 		List<String> results = new ArrayList<String>();
 		for(int i=0 ; i<leftEvent.size() ; i++){
